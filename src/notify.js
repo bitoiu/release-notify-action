fa0beb41788fee7205321e990ceef01dcb856583
Tom Sugden <tom.sugden@gmail.com>
2018-10-22 21:49:04 +0000

    add missing comma

--- src/notify.js
+++ src/notify.js
@@ -21,21 +21,19 @@ let prepareMessage = function(recipients) {
     releaseVersion = eventPayload.release.tag_name,
     releaseName = eventPayload.release.name,
     releaseURL = eventPayload.release.html_url,
-    newIssueURL = eventPayload.repository.html_url + "/issues/new"
+    newIssueURL = eventPayload.repository.html_url + "/issues/new",
 
-  // This is not efficient but I find it quite readable
-  emailSubject = SUBJECT_TEMPLATE
-    .replace("$REPO$", repoName)
-    .replace("$VERSION$", releaseVersion)
-    .replace("$NAME$", releaseName),
-
+    // This is not efficient but I find it quite readable
+    emailSubject = SUBJECT_TEMPLATE
+      .replace("$REPO$", repoName)
+      .replace("$VERSION$", releaseVersion)
+      .replace("$NAME$", releaseName),
 
     footer = FOOTER_TEMPLATE
       .replace("$RELEASEURL$", releaseURL)
       .replace("$NEWISSUEURL$", newIssueURL),
 
     releaseBody = converter.makeHtml(eventPayload.release.body + footer)
-
 
   let msg = {
     to: ['subscribers@no-reply.com'],
